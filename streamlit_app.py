8f6d25fdc128c3cbbc4aa35cc686f39d6c28b692
Varshini1401 <109915510+Varshini1401@users.noreply.github.com>
2024-04-02 11:47:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import LabelEncoder
 import pickle
 
 # Load the trained machine learning model
-with open('Project/base.pkl', 'rb') as f:  # Replace 'your_model_file.pkl' with the path to your trained model file
+with open('base.pkl', 'rb') as f:  # Replace 'your_model_file.pkl' with the path to your trained model file
     model = pickle.load(f)
 data=pd.read_csv(r"C:\Users\Varshini\Downloads\Disease_symptom_and_patient_profile_dataset (1).csv")
 # Define the function to preprocess input data
